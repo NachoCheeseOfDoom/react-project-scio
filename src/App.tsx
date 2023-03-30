c373b912f968a46233b66063dba0080a1ee27be3
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2023-03-30 10:45:44 -0600

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,6 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link> */}
               <Link className="text-secondary me-3" to="/test">
                 Test
               </Link>
@@ -35,24 +33,12 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to="/test">Test</Link>
-          </li>
-        </ul>
-      </nav> */}
       <div className="App">
-        {/* <header className="App-header"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <AlertButton alertText="" /> */}
-
         <Routes>
           <Route path="/test" element={<TestPage />} />
           <Route path="/404" element={<PageNotFound />} />
           <Route path="/" element={<ApiTest />} />
         </Routes>
-        {/* </header> */}
       </div>
     </>
   );
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -4,24 +4,7 @@ import AlertButton from "../components/AlertButton";
 export default function TestPage() {
   return (
     <>
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
       <AlertButton BtnText="Click for alert message" />
-      {/* <div>TestPage, i can add something else here</div> */}
-      {/* <form action="https://google.com">
-        <input
-          className="btn btn-primary mt-2 mb-2"
-          type="submit"
-          value="Go to Google"
-        />
-      </form>
-      <form action="https://youtube.com">
-        <input
-          className="btn btn-primary mt-2 mb-2"
-          type="submit"
-          value="Go to Youtube"
-        />
-      </form> */}
     </>
   );
 }
